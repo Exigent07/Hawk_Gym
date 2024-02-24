04eff88de12106ba44f79fa33a398ffb9942f889
Exigent07 <aravindhlearner07@gmail.com>
2024-02-24 19:42:35 +0530

    Completed register and login

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const express       = require('express');
+const ejs           = require('ejs');
+const bodyParser    = require('body-parser');
+const cookieParser  = require('cookie-parser');
+const mysql         = require('mysql');
+const bcrypt        = require('bcrypt');
+const {encode}  = require('html-entities');
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app   = express();
+const port  = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
@@ -74,49 +75,47 @@ Object.values(query).forEach(element => {
   });
 });
 
-app.get("/login", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
+  const input = req.body.username;
   const password = req.body.password;
 
-  let query   = `SELECT * FROM users WHERE username = ?`;
-  let values  = [username];
+  let query   = `SELECT * FROM users WHERE username = ? OR email = ?`;
+  let values  = [input, input];
   con.query(query, values, async (error, results, fields) => {
     if (error) {
       console.error('Error executing query:', error);
       return;
     }
     console.log('Query results:', results);
+    if (results.length === 0) {
+      res.send("Invalid Username or Password");
+      return;
+    }
     let hashedPassword = results[0].password;
     let match          = await comparePasswords(password, hashedPassword);
     console.log(match);
     if (match) {
-      res.send(`Username: ${username}<br>Password: ${password}`);
+      res.send(`Username: ${results[0].username}<br>Password: ${password}`);
     } else {
       res.send("Invalid Username or Password");
     }
+    
   });
 });
 
-app.get("/register", (req, res) => {
-  res.send(`    <form action="/register" method="post">
-  <input type="text" name="username" required>
-  <input type="password" name="password" required>
-  <button type="submit">Submit</button>
-</form>`);
-});
-
 app.post("/register", async (req, res) => {
-  const username = req.body.username;
+  const username = encode(req.body.username);
   const password = req.body.password;
+  const email    = req.body.email;
 
   try {
     const hashedPass = await hashPassword(password);
-    const query = `INSERT INTO users(username, password) VALUES (?, ?)`;
-    con.query(query, [username, hashedPass], (err, result) => {
+    const query = `INSERT INTO users(username, password, email) VALUES (?, ?, ?)`;
+    con.query(query, [username, hashedPass, email], (err, result) => {
       if (err) {
         console.error('Error registering user:', err);
         res.status(500).send('Error registering user');
